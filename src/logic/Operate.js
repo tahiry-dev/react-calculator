b462f1313a7c261c6ce2992a8bcdddcb88b54d9b
tahiry-dev <tahirycontact@gmail.com>
2021-04-21 19:09:58 +0300

    fix eslint error

--- src/logic/Operate.js
+++ src/logic/Operate.js
@@ -1,5 +1,13 @@
 import Big from 'big.js';
 
+const plus = (a, b) => `${a.plus(b)}`;
+
+const subtract = (a, b) => a.minus(b).valueOf();
+
+const divide = (a, b) => (`${b}` === '0' ? 'INFINITY' : a.div(b).valueOf());
+
+const multiply = (a, b) => a.times(b).valueOf();
+
 const Operate = (numberOne, numberTwo, operation) => {
   const numOne = Big(numberOne);
   const numTwo = Big(numberTwo);
